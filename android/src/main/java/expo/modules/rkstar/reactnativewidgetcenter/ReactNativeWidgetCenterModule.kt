71d7c0b0d2028be3005b7b863668815add4e9b4d
David Fudge <rkstar@mac.com>
2023-10-24 10:08:54 -0400

    fix android module definition

--- android/src/main/java/expo/modules/rkstar/reactnativewidgetcenter/ReactNativeWidgetCenterModule.kt
+++ android/src/main/java/expo/modules/rkstar/reactnativewidgetcenter/ReactNativeWidgetCenterModule.kt
@@ -1,4 +1,4 @@
-package expo.modules.widgetcenter
+package expo.modules.rkstar.reactnativewidgetcenter
 
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
